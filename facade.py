b49572342a5e3d733d303d115c560ae964d01ad3
Sergio Prolo <smauricioj@gmail.com>
2025-02-20 15:25:15 -0300

    Primeira versão do projeto padrão Facade

---
+++ facade.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+
+class Facade:
+    '''
+    A Facade forcene uma interface simplificada para um
+    conjunto completo de subsistemas. Isso barra o acesso
+    do cliente aos subsistemas e simplifica o seu entendimento
+    '''
+    
+    def __init__(self) -> None:
+        '''
+        Os subsistemas estão aqui instanciados na
+        inicialização, mas também podem ser passados como
+        argumentos se necessário
+        '''
+        self._subsystemA: SubsystemA = SubsystemA()
+        self._subsystemB: SubsystemB = SubsystemB()
+        
+    def operation(self) -> bool:
+        '''
+        Uma operação complexa que o cliente fica
+        abstraído e separado
+        '''
+        results: list = []
+        results.append(self._subsystemA.operation1())
+        results.append(self._subsystemA.operation2())
+        results.append(self._subsystemB.operation3())
+        results.append(self._subsystemB.operation4())
+        return all(results)
+
+
+class SubsystemA:
+    '''
+    Um subsistema com diversas operações
+    '''
+    
+    def operation1(self) -> bool:
+        return True
+    
+    def operation2(self) -> bool:
+        return True
+
+
+class SubsystemB:
+    '''
+    Outro subsistema, que organização complexa!
+    '''
+    
+    def operation3(self) -> bool:
+        return True
+    
+    def operation4(self) -> bool:
+        return True
+    
+# Testes
+def facade_test() -> bool:
+    '''
+    O código do cliente é super simples
+    '''
+    facade = Facade()
+    assert(facade.operation())
+    
+# Main
+def main() -> None:
+    assert(facade_test())
+    
+if __name__ == "__main__":
+    main()
+        
